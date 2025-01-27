beaf2e88d654b2be9be40c79e3752b56aaf86eb3
CPezz27 <divicocarlo97@gmail.com>
2025-01-27 23:06:04 +0100

    Update lesion_models.py

--- lesion_models.py
+++ lesion_models.py
@@ -1,7 +1,6 @@
 import os
 import numpy as np
 import cv2
-from sklearn.model_selection import train_test_split
 import segmentation_models_pytorch as smp
 import torch
 import torch.optim as optim
@@ -60,7 +59,15 @@ def combine_image_with_mask(image, mask):
 
 def tversky_loss(y_pred, y_true, alpha=0.6, beta=0.4, smooth=1e-6, class_weights=None):
 
+    print(f"y_pred: {y_pred.shape}")
+
+    print(f"y_true: {y_true.shape}")
+
     y_true = (y_true > 0.5).float()
+
+    print(f"y_pred: {y_pred.shape}")
+
+    print(f"y_true: {y_true.shape}")
 
     tp = (y_pred * y_true).sum(dim=(2, 3))
     fp = ((1 - y_true) * y_pred).sum(dim=(2, 3))
@@ -69,8 +76,6 @@ def tversky_loss(y_pred, y_true, alpha=0.6, beta=0.4, smooth=1e-6, class_weights
 
     if class_weights is not None:
         tversky_index *= class_weights
-
-    print(f"tversky_index: {tversky_index.shape}")
 
     return 1 - tversky_index.mean()
 
@@ -114,8 +119,6 @@ def visualize_predictions(images_batch, masks_batch, outputs):
 def calculate_class_weights(loader, num_classes):
     class_counts = torch.zeros(num_classes, dtype=torch.float32)
 
-    print(f"Class counts: {class_counts}")
-
     for _, masks_batch in loader:
         masks_batch = masks_batch.view(masks_batch.size(0), -1, num_classes)
         for c in range(num_classes):
@@ -129,22 +132,18 @@ def calculate_class_weights(loader, num_classes):
     return class_weights / class_weights.sum()
 
 
-'''        
-da aggiungere dopo il primo for nella funzione sopra
-# print(f"mask batch before: {masks_batch}")
-masks_batch = masks_batch.view(-1)
-# print(f"mask batch after: {masks_batch}")
-'''
-
-
 def preprocess_data(image_dir, mask_dir, lesion_types, target_size=(128, 128)):
+
     combined_inputs = []
+    images = []
     combined_masks = []
+    masks = []
 
     for filename in os.listdir(image_dir):
         img_path = os.path.join(image_dir, filename)
         img = cv2.imread(img_path)
         img = cv2.resize(img, target_size) / 255.0
+        images.append(img)
         mask_stack = []
 
         for lesion_type in lesion_types:
@@ -153,7 +152,6 @@ def preprocess_data(image_dir, mask_dir, lesion_types, target_size=(128, 128)):
 
             if os.path.exists(mask_path):
                 mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
-                # mask = cv2.resize(mask, target_size) / 255.0
                 mask = (cv2.resize(mask, target_size) > 0).astype(np.float32)
             else:
                 mask = np.zeros(target_size)
@@ -163,25 +161,26 @@ def preprocess_data(image_dir, mask_dir, lesion_types, target_size=(128, 128)):
         combined_input = combine_image_with_mask(img, mask_stack)
         combined_inputs.append(combined_input)
 
-        combined_masks.append(np.stack(mask_stack, axis=-1))
+        masks.append(np.stack(mask_stack, axis=-1))
 
-    return np.array(combined_inputs), np.array(combined_masks)
+        for combined_mask in masks:
+            combined_mask = (cv2.resize(combined_mask, target_size) > 0).astype(np.float32)
+            combined_masks.append(combined_mask)
+
+    return np.array(images), np.array(combined_inputs), np.array(combined_masks)
 
 
-images, combined_masks = preprocess_data(image_dir, mask_dir, lesion_mask_types)
-
-train_images, val_images, train_combined_masks, val_combined_mask = train_test_split(images, combined_masks,
-                                                                                     test_size=0.2, random_state=42)
+images, combined_mask_images, combined_masks = preprocess_data(image_dir, mask_dir, lesion_mask_types)
 
 k = 5
-num_classes = len(lesion_mask_types)
+num_classes = 2
 kf = KFold(n_splits=k, shuffle=True, random_state=42)
 
 lesion_model = smp.DeepLabV3Plus(
     encoder_name='resnet50',
     encoder_weights='imagenet',
     in_channels=3 + len(lesion_mask_types),
-    classes=len(lesion_mask_types),
+    classes=2,
     activation=None
 ).to(device)
 
@@ -200,11 +199,14 @@ optimizer = optim.Adam(lesion_model.parameters(), lr=1e-3, weight_decay=1e-4)
 scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=5, verbose=True)
 
 num_epochs = 50
-for fold, (train_idx, val_idx) in enumerate(kf.split(images)):
+for fold, (train_idx, val_idx) in enumerate(kf.split(combined_mask_images)):
     print(f"Fold {fold + 1}/{k}")
 
-    train_images, val_images = images[train_idx], images[val_idx]
+    train_images, val_images = combined_mask_images[train_idx], combined_mask_images[val_idx]
     train_combined_masks, val_combined_masks = combined_masks[train_idx], combined_masks[val_idx]
+
+    print(f"train_images: {len(train_images)}, val_images: {len(val_images)}, "
+          f"train_combined_masks: {len(train_combined_masks)}, val_combined_mask: {len(val_combined_masks)}")
 
 # debug
     train_combined_masks_tensor = torch.tensor(train_combined_masks)
